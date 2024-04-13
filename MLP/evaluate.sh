67dc8ea1b624d37ed6c752e04cce64dda9d7fcc6
Helen Li <hallenljy@gmail.com>
2024-04-13 16:21:33 -0400

    initial evaluate script

---
+++ MLP/evaluate.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+cd Regression
+
+# perf
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 2 --model Monolithic --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 4 --model Monolithic --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 8 --model Monolithic --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 16 --model Monolithic --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 32 --model Monolithic --num-rules 32
+
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 2 --model Modular --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 4 --model Modular --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 8 --model Modular --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 16 --model Modular --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 32 --model Modular --num-rules 32
+
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 2 --model Modular --num-rules 2 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 4 --model Modular --num-rules 4 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 8 --model Modular --num-rules 8 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 16 --model Modular --num-rules 16 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 32 --model Modular --num-rules 32 --op --joint
+
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 2 --model GT_Modular --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 4 --model GT_Modular --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 8 --model GT_Modular --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 16 --model GT_Modular --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 32 --model GT_Modular --num-rules 32
+
+# spec
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 2 --model Monolithic --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 4 --model Monolithic --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 8 --model Monolithic --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 16 --model Monolithic --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 32 --model Monolithic --num-rules 32
+
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 2 --model Modular --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 4 --model Modular --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 8 --model Modular --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 16 --model Modular --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 32 --model Modular --num-rules 32
+
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 2 --model Modular --num-rules 2 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 4 --model Modular --num-rules 4 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 8 --model Modular --num-rules 8 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 16 --model Modular --num-rules 16 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 32 --model Modular --num-rules 32 --op --joint
+
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 2 --model GT_Modular --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 4 --model GT_Modular --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 8 --model GT_Modular --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 16 --model GT_Modular --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 32 --model GT_Modular --num-rules 32
+
+# prob
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 2 --model Monolithic --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 4 --model Monolithic --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 8 --model Monolithic --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 16 --model Monolithic --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 32 --model Monolithic --num-rules 32
+
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 2 --model Modular --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 4 --model Modular --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 8 --model Modular --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 16 --model Modular --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 32 --model Modular --num-rules 32
+
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 2 --model Modular --num-rules 2 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 4 --model Modular --num-rules 4 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 8 --model Modular --num-rules 8 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 16 --model Modular --num-rules 16 --op --joint
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 32 --model Modular --num-rules 32 --op --joint
+
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 2 --model GT_Modular --num-rules 2
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 4 --model GT_Modular --num-rules 4
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 8 --model GT_Modular --num-rules 8
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 16 --model GT_Modular --num-rules 16
+srun -p csc413 --gres gpu -c 2 --pty python3 prob.py --gt-rules 32 --model GT_Modular --num-rules 32
+
+# evaluate all found models
+# srun -p csc413 --gres gpu -c 2 --pty python3 metrics.py
+# srun -p csc413 --gres gpu -c 2 --pty python3 hungarian.py
