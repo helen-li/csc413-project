116d80f06d0f177f38f77f56501ff3505b6ab523
Aidan Li <aidan.mr.li@gmail.com>
2024-04-15 03:14:04 -0400

    bugfix for uncommented command

--- MLP/Classification/evaluate.sh
+++ MLP/Classification/evaluate.sh
@@ -25,7 +25,7 @@ srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 8 --model GT_Mod
 srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 16 --model GT_Modular --num-rules 16
 srun -p csc413 --gres gpu -c 2 --pty python3 perf.py --gt-rules 32 --model GT_Modular --num-rules 32
 
-spec
+# spec
 srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 2 --model Monolithic --num-rules 2
 srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 4 --model Monolithic --num-rules 4
 srun -p csc413 --gres gpu -c 2 --pty python3 spec.py --gt-rules 8 --model Monolithic --num-rules 8
